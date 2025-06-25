a21b667719858bd0df563f90b31c2b66c0c85374
krishangoyal12 <krishangoyal717@gmail.com>
2025-06-25 12:40:27 +0530

    completed

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ProjectBox from './ProjectBox';
-import NewsletterImage from '../images/NewsletterImage.png';
-import RogfreeImage from '../images/RogfreeImage.png';
-import TindogImage from '../images/TindogImage.png';
-import WigglesImage from '../images/WigglesImage.png';
+import SkillHireImage from '../images/SkillHire.png'
+import CoWriteImage from '../images/CoWrite.png';
+import EmotiSenseImage from '../images/EmotiSense.png';
+import apiwizImage from '../images/apiwiz.png';
 
 const Projects = () => {
   return (
     <div>
       <h1 className='projectHeading'>My <b>Projects</b></h1>
       <div className='project'>
-        <ProjectBox projectPhoto={WigglesImage} projectName="SkillHire" />
-        <ProjectBox projectPhoto={NewsletterImage} projectName="CoWrite" />
-        <ProjectBox projectPhoto={RogfreeImage} projectName="EmotiSense" />
-        <ProjectBox projectPhoto={TindogImage} projectName="apiwiz" />
+        <ProjectBox projectPhoto={SkillHireImage} projectName="SkillHire" />
+        <ProjectBox projectPhoto={CoWriteImage} projectName="CoWrite" />
+        <ProjectBox projectPhoto={EmotiSenseImage} projectName="EmotiSense" />
+        <ProjectBox projectPhoto={apiwizImage} projectName="apiwiz" />
       </div>
 
     </div>
