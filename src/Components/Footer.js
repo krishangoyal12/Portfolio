4736238742cea44a32c10d32eab339cba68ae9fd
Krishan Goyal <89409398+krishangoyal12@users.noreply.github.com>
2025-07-03 12:11:57 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
       <h4>Developed by Krishan Goyal</h4>
       <h4>Copyright &copy; 2025 KG</h4>
       <div className='footerLinks'>
-        <a href="https://github.com/krishangoyal12" target='_blank'><FaGithub/></a>
+        <a href="https://github.com/krishangoyal12" target='_blank'><FaGithub/></a> 
         <a href="https://www.linkedin.com/in/krishangoyal717/" target='_blank'><FaLinkedin/></a>
         <a href='mailTo:krishangoyal717@gmail.com' target='_blank'><GrMail/></a>
         <a href="https://leetcode.com/u/krishangoyal12/" target="_blank"><SiLeetcode/></a>
